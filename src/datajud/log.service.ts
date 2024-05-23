510953e2459f8c0cd624a729695ac7ae57325d23
gustavorolan <gustavorolan@live.com>
2024-05-23 16:32:35 -0300

    travis commit

--- src/datajud/log.service.ts
+++ src/datajud/log.service.ts
@@ -2,8 +2,8 @@ import { Injectable, Logger } from '@nestjs/common';
 
 @Injectable()
 export class LogService {
-  errorAndThrow(logger: Logger, exception: Error) {
-    logger.error(JSON.stringify(exception));
-    throw exception;
+  errorAndThrow(logger: Logger, e: Error) {
+    logger.error(JSON.stringify(e));
+    throw e;
   }
 }
